8f04a8bff15242bb94edb90c8fcd15661cc0aa37
Trevor Mwangi <trvrmwangi@gmail.com>
2024-09-05 18:15:27 +0300

    email configuration

--- dailyemail.py
+++ dailyemail.py
@@ -1,1 +1,15 @@
 #pip install schedule
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import schedule
+import time
+from datetime import datetime
+
+# Function to send email
+def send_email():
+    # Email configuration
+    sender_email = "willfrazier715@gmail.com"
+    receiver_email = "trevormwangi94@gmail.com"
+    password = "31557225009"
